18be8b0bff5a903056876e096bedb2c5efdca581
Nova Arctic <nova@zeusteam.dev>
2025-02-20 22:29:15 +1000

    Add security features to trace proxies and check the traffic

--- index.js
+++ index.js
@@ -7,24 +7,44 @@ const app = express();
 const proxy = createProxyMiddleware({
   changeOrigin: true,
   secure: true,
-  logLevel: 'debug',
+  logLevel: 'silent',
   router: function(req) {
-    const targetUrl = req.query.url; 
+    const targetUrl = req.query.url;
     if (targetUrl) {
-      return targetUrl; 
+      const forwardedfor = req.headers['x-forwarded-for'];
+      const ipregex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+      if (!forwardedfor || !ipregex.test(forwardedfor)) {
+        res.end();
+        return;
+      }
     }
-    return 'https://xivi.org'; 
+    return 'https://xivi.org';
   },
   onProxyReq: (proxyReq, req, res) => {
-    
-    proxyReq.setHeader('X-Forwarded-For', ''); 
-    proxyReq.setHeader('X-Real-IP', '');
-    proxyReq.setHeader('Via', '');
+    const clientip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const proxyip = req.socket.localAddress;
+    proxyReq.setHeader('X-Forwarded-For', clientip);
+    proxyReq.setHeader('X-Proxy-Hostname', req.hostname);
+    proxyReq.setHeader('X-Proxy-IP', proxyip);
+    proxyReq.setHeader('X-Proxy-Contact', process.env.CONTACT || process.env.VERCEL_BRANCH_URL || process.env.VERCEL_URL || 'Contact information not available');
+  },
+  onError: (err, req, res) => {
+    console.error('Proxy error:', err);
+    res.writeHead(500, {
+      'Content-Type': 'text/plain'
+    });
+    res.end('Something went very wrong.');
   }
 });
 
 app.use('/', proxy);
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`Xivi 15 is running on port ${port}`);
+  if (!process.env.CONTACT) {
+    console.warn('!!! Your Xivi reverse proxy is running without a contact method. Please set the CONTACT environment variable to your Discord username or email address. !!!');
+    console.warn('!!! If you do not set a contact method, your proxy may be blocked without warning. !!!');
+  } else {
+    console.log(`Contact is set to: ${process.env.CONTACT}`);
+  }
+  console.log(`Xivi reverse proxy is running on port ${port} (started at ${new Date().toISOString()})`);
 });
